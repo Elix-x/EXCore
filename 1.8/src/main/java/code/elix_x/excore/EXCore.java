fb1466107f0fe495daa45cd8ab95d5c4cdff917b
elix-x <agent-lego@mail.ru>
2015-12-13 12:58:59 +0100

    1.3.1 - Additions, improvements, cleanup.
    -Some additions that do not require major version: ListUtils.
    -Improvements: SpecialArrayUtils.
    -Code cleanup.

--- 1.8/src/main/java/code/elix_x/excore/EXCore.java
+++ 1.8/src/main/java/code/elix_x/excore/EXCore.java
@@ -7,8 +7,8 @@ public class EXCore {
 
 	public static final String MODID = "excore";
 	public static final String NAME = "EXCore";
-	public static final String VERSION = "1.3";
-	
+	public static final String VERSION = "1.3.1";
+
 	public static final String DEPENDENCY = MODID + "@[" + VERSION + ",)";
-	
+
 }
