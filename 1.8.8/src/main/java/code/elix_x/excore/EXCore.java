e93549773b2c01056cd2c02bc56dd0d6aabca4ee
elix-x <agent-lego@mail.ru>
2015-12-13 13:39:47 +0100

    1.8.8
    Versions for minecraft 1.8.8 are here.

---
+++ 1.8.8/src/main/java/code/elix_x/excore/EXCore.java
@@ -0,0 +1,14 @@
+package code.elix_x.excore;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = EXCore.MODID, name = EXCore.NAME, version = EXCore.VERSION, acceptedMinecraftVersions = "1.8.8")
+public class EXCore {
+
+	public static final String MODID = "excore";
+	public static final String NAME = "EXCore";
+	public static final String VERSION = "1.3.1";
+
+	public static final String DEPENDENCY = MODID + "@[" + VERSION + ",)";
+
+}
