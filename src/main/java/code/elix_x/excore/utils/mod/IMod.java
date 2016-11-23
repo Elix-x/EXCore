788c9cc2502c426fe65c22c53b2a982bcde47b30
elix-x <Elix-x@users.noreply.github.com>
2016-11-23 20:35:36 +0100

    Removed all classes

--- src/main/java/code/elix_x/excore/utils/mod/IMod.java
+++
@@ -1,9 +0,0 @@
-package code.elix_x.excore.utils.mod;
-
-import code.elix_x.excore.utils.proxy.IProxy;
-
-public interface IMod<M extends IMod<M, P>, P extends IProxy<M>> extends IProxy {
-
-	public P getProxy();
-
-}
