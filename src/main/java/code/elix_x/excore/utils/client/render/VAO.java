844a238fd9baabde28f3dc82817a205fcdfd0897
elix-x <Elix-x@users.noreply.github.com>
2016-12-28 22:19:46 +0100

    Added methods to data bindings to VOs
    Added buffer binding method to VBO.
    Added VBO binding methods to VAO.

--- src/main/java/code/elix_x/excore/utils/client/render/VAO.java
+++ src/main/java/code/elix_x/excore/utils/client/render/VAO.java
@@ -1,28 +1,39 @@
 package code.elix_x.excore.utils.client.render;
 
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
-import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.client.renderer.vertex.VertexFormatElement;
+import net.minecraft.client.renderer.vertex.VertexFormatElement.EnumUsage;
 
 public class VAO {
 
 	private final int vaoId;
-	
+
 	public VAO(){
 		this.vaoId = GL30.glGenVertexArrays();
 	}
-	
+
 	public void bind(){
 		GL30.glBindVertexArray(vaoId);
 	}
-	
+
+	public void vboSeparate(VBO vbo, VertexFormatElement format){
+		GL20.glVertexAttribPointer(0, format.getElementCount(), format.getType().getGlConstant(), format.getUsage() == EnumUsage.NORMAL, 0, 0);
+	}
+
+	public void vboSingle(VBO vbo, VertexFormat format, VertexFormatElement elementFormat){
+		GL20.glVertexAttribPointer(0, elementFormat.getElementCount(), elementFormat.getType().getGlConstant(), elementFormat.getUsage() == EnumUsage.NORMAL, format.getIntegerSize(), format.getOffset(elementFormat.getIndex()));
+	}
+
 	public void unbind(){
 		GL30.glBindVertexArray(0);
 	}
-	
+
 	public void cleanUp(){
 		unbind();
 		GL30.glDeleteVertexArrays(vaoId);
 	}
-	
+
 }
--- src/main/java/code/elix_x/excore/utils/client/render/VBO.java
+++ src/main/java/code/elix_x/excore/utils/client/render/VBO.java
@@ -1,5 +1,7 @@
 package code.elix_x.excore.utils.client.render;
 
+import java.nio.ByteBuffer;
+
 import org.lwjgl.opengl.GL15;
 
 public class VBO {
@@ -12,6 +14,10 @@ public class VBO {
 
 	public void bind(){
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
+	}
+
+	public void data(ByteBuffer buffer, int usage){
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, usage);
 	}
 
 	public void unbind(){
