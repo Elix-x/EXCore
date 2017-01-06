5183869e88c38de33bcf89ae901f5c0786845934
elix-x <Elix-x@users.noreply.github.com>
2017-01-06 21:42:37 +0100

    Fixed BAR based on experimentation

--- src/main/java/code/elix_x/excore/utils/client/render/world/BlockAccessRenderer.java
+++ src/main/java/code/elix_x/excore/utils/client/render/world/BlockAccessRenderer.java
@@ -9,11 +9,16 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.MinecraftForgeClient;
 
 public class BlockAccessRenderer {
 
@@ -40,30 +45,41 @@ public class BlockAccessRenderer {
 			rebuildBuffers();
 			needsUpdate = false;
 		}
+		OpenGLHelper.enableClientState(DefaultVertexFormats.BLOCK);
+		Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+		RenderHelper.disableStandardItemLighting();
+		Minecraft.getMinecraft().entityRenderer.enableLightmap();
+		for(BlockRenderLayer layer : BlockRenderLayer.values()){
+			//TODO layer specific GL setups
+			vertexBuffers[layer.ordinal()].draw();
+		}
+		Minecraft.getMinecraft().entityRenderer.disableLightmap();
+		RenderHelper.enableStandardItemLighting();
+		OpenGLHelper.disableClientState(DefaultVertexFormats.BLOCK);
 	}
 
 	protected void rebuildBuffers(){
 		cleanUp();
+		BlockRenderLayer prev = MinecraftForgeClient.getRenderLayer();
 		for(BlockRenderLayer layer : BlockRenderLayer.values()){
-			net.minecraft.client.renderer.VertexBuffer buffer = new net.minecraft.client.renderer.VertexBuffer(16);
+			net.minecraft.client.renderer.VertexBuffer buffer = Tessellator.getInstance().getBuffer();
 			BlockRendererDispatcher blockRenderer = Minecraft.getMinecraft().getBlockRendererDispatcher();
 			for(int x = (int) shape.minX; x < shape.maxX; x++){
 				for(int y = (int) shape.minY; y < shape.maxY; y++){
 					for(int z = (int) shape.minZ; z < shape.maxZ; z++){
 						BlockPos pos = new BlockPos(x, y, z);
 						IBlockState state = world.getBlockState(pos);
-						if(state.getBlock().canRenderInLayer(state, layer))
+						if(state.getBlock().canRenderInLayer(state, layer)){
+							ForgeHooksClient.setRenderLayer(layer);
 							blockRenderer.renderBlock(state, pos, world, buffer);
+						}
 					}
 				}
 			}
+			buffer.finishDrawing();
 			vertexBuffers[layer.ordinal()] = new VertexBufferSingleVBO(buffer).setModifyClientStates(false);
 		}
-		OpenGLHelper.enableClientState(DefaultVertexFormats.BLOCK);
-		for(BlockRenderLayer layer : BlockRenderLayer.values()){
-			vertexBuffers[layer.ordinal()].draw();
-		}
-		OpenGLHelper.disableClientState(DefaultVertexFormats.BLOCK);
+		ForgeHooksClient.setRenderLayer(prev);
 	}
 
 	protected void cleanUp(){
