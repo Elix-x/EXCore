9e50bba495cc5be2949550dbf29d31db88ad3d39
elix-x <Elix-x@users.noreply.github.com>
2017-01-19 20:28:03 +0100

    Imported baking pipeline from AE
    Imported baking pipeline with all classes and interfaces from AE.

---
+++ src/main/java/code/elix_x/excore/utils/client/render/pipeline_old/BakingPipelineElement.java
@@ -0,0 +1,13 @@
+package code.elix_x.excore.utils.client.render.pipeline_old;
+
+import java.util.List;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.util.EnumFacing;
+
+public interface BakingPipelineElement<F, T> {
+
+	public List<T> pipe(List<F> elements, IBakedModel parent, IBlockState state, EnumFacing side, long rand);
+
+}
