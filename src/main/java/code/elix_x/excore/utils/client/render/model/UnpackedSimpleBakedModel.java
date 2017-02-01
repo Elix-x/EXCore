70f9bae41ade6c004f564965ddcbc46e154ebb6e
elix-x <Elix-x@users.noreply.github.com>
2017-02-01 20:39:12 +0100

    Implemented unpacked simple baked model unpacking

--- src/main/java/code/elix_x/excore/utils/client/render/model/UnpackedSimpleBakedModel.java
+++ src/main/java/code/elix_x/excore/utils/client/render/model/UnpackedSimpleBakedModel.java
@@ -2,6 +2,7 @@ package code.elix_x.excore.utils.client.render.model;
 
 import java.util.List;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multimaps;
@@ -91,6 +92,13 @@ public class UnpackedSimpleBakedModel {
 
 	public SimpleBakedModel pack(VertexFormat format){
 		return new SimpleBakedModel(Lists.transform(generalQuads, quad -> quad.pack(format)), Multimaps.asMap(Multimaps.transformValues(faceQuads, quad -> quad.pack(format))), ambientOcclusion, gui3d, texture, cameraTransforms, itemOverrideList);
+	}
+
+	public static UnpackedSimpleBakedModel unpack(SimpleBakedModel model){
+		ListMultimap<EnumFacing, UnpackedBakedQuad> faceQuads = ArrayListMultimap.create();
+		for(EnumFacing facing : EnumFacing.values())
+			faceQuads.putAll(facing, Lists.transform(model.getQuads(null, facing, 0), quad -> UnpackedBakedQuad.unpack(quad)));
+		return new UnpackedSimpleBakedModel(Lists.transform(model.getQuads(null, null, 0), quad -> UnpackedBakedQuad.unpack(quad)), faceQuads, model.isAmbientOcclusion(), model.isGui3d(), model.getParticleTexture(), model.getItemCameraTransforms(), model.getOverrides());
 	}
 
 }
