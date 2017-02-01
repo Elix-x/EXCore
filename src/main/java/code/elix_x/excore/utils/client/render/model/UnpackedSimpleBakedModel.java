d87196708a539c4cf3ec13828d2cc575496730d1
elix-x <Elix-x@users.noreply.github.com>
2017-02-01 20:25:01 +0100

    Implemeted simple baked model packing

--- src/main/java/code/elix_x/excore/utils/client/render/model/UnpackedSimpleBakedModel.java
+++ src/main/java/code/elix_x/excore/utils/client/render/model/UnpackedSimpleBakedModel.java
@@ -3,10 +3,14 @@ package code.elix_x.excore.utils.client.render.model;
 import java.util.List;
 
 import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimaps;
 
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.block.model.ItemOverrideList;
+import net.minecraft.client.renderer.block.model.SimpleBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.util.EnumFacing;
 
 public class UnpackedSimpleBakedModel {
@@ -83,6 +87,10 @@ public class UnpackedSimpleBakedModel {
 
 	public void setItemOverrideList(ItemOverrideList itemOverrideList){
 		this.itemOverrideList = itemOverrideList;
+	}
+
+	public SimpleBakedModel pack(VertexFormat format){
+		return new SimpleBakedModel(Lists.transform(generalQuads, quad -> quad.pack(format)), Multimaps.asMap(Multimaps.transformValues(faceQuads, quad -> quad.pack(format))), ambientOcclusion, gui3d, texture, cameraTransforms, itemOverrideList);
 	}
 
 }
